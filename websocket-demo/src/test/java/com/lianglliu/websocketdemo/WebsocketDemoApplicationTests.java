133c7998fb8f626a786f79f8de4fe8a004948816
LiangLliu <max1141847753@gmail.com>
2022-01-06 23:58:29 +0800

    初始化 websocket-demo

---
+++ websocket-demo/src/test/java/com/lianglliu/websocketdemo/WebsocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lianglliu.websocketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
